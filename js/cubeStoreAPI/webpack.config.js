6042ce03af14dbd93091a174b1bdf52253697d99
nicolasrannou <npm.rannou@gmail.com>
2018-06-06 18:23:27 +0200

    Initial cubeStoreAPI setup

---
+++ js/cubeStoreAPI/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    // Change to your "entry-point".
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'chrisStoreAPI.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, and js files.
+            test: /\.(tsx?)|(js)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    }
+};
