4a5abbf58d1b2f40ae27829e00d864c3c640453e
nicolasrannou <npm.rannou@gmail.com>
2018-06-07 22:32:26 +0200

    Support async testing

--- js/cubeStoreAPI/dist/chrisStoreAPI.js
+++ js/cubeStoreAPI/dist/chrisStoreAPI.js
@@ -1,1 +1,110 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";function n(){return"Hello World!"}Object.defineProperty(t,"__esModule",{value:!0}),t.hello=n,t.default=void 0;var o=n;t.default=o}]);
+!(function(e) {
+  var t = {};
+  function r(n) {
+    if (t[n]) return t[n].exports;
+    var o = (t[n] = { i: n, l: !1, exports: {} });
+    return e[n].call(o.exports, o, o.exports, r), (o.l = !0), o.exports;
+  }
+  (r.m = e),
+    (r.c = t),
+    (r.d = function(e, t, n) {
+      r.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: n });
+    }),
+    (r.r = function(e) {
+      'undefined' != typeof Symbol &&
+        Symbol.toStringTag &&
+        Object.defineProperty(e, Symbol.toStringTag, { value: 'Module' }),
+        Object.defineProperty(e, '__esModule', { value: !0 });
+    }),
+    (r.t = function(e, t) {
+      if ((1 & t && (e = r(e)), 8 & t)) return e;
+      if (4 & t && 'object' == typeof e && e && e.__esModule) return e;
+      var n = Object.create(null);
+      if (
+        (r.r(n),
+        Object.defineProperty(n, 'default', { enumerable: !0, value: e }),
+        2 & t && 'string' != typeof e)
+      )
+        for (var o in e)
+          r.d(
+            n,
+            o,
+            function(t) {
+              return e[t];
+            }.bind(null, o)
+          );
+      return n;
+    }),
+    (r.n = function(e) {
+      var t =
+        e && e.__esModule
+          ? function() {
+              return e.default;
+            }
+          : function() {
+              return e;
+            };
+      return r.d(t, 'a', t), t;
+    }),
+    (r.o = function(e, t) {
+      return Object.prototype.hasOwnProperty.call(e, t);
+    }),
+    (r.p = ''),
+    r((r.s = 0));
+})([
+  function(e, t, r) {
+    'use strict';
+    function n() {
+      return o.apply(this, arguments);
+    }
+    function o() {
+      var e;
+      return (
+        (e = regeneratorRuntime.mark(function e() {
+          return regeneratorRuntime.wrap(
+            function(e) {
+              for (;;)
+                switch ((e.prev = e.next)) {
+                  case 0:
+                    return (e.next = 2), window.console.log('hi');
+                  case 2:
+                    return e.abrupt('return', 'Hello World!');
+                  case 3:
+                  case 'end':
+                    return e.stop();
+                }
+            },
+            e,
+            this
+          );
+        })),
+        (o = function() {
+          var t = this,
+            r = arguments;
+          return new Promise(function(n, o) {
+            var u = e.apply(t, r);
+            function i(e, t) {
+              try {
+                var r = u[e](t),
+                  i = r.value;
+              } catch (e) {
+                return void o(e);
+              }
+              r.done ? n(i) : Promise.resolve(i).then(a, c);
+            }
+            function a(e) {
+              i('next', e);
+            }
+            function c(e) {
+              i('throw', e);
+            }
+            a();
+          });
+        }).apply(this, arguments)
+      );
+    }
+    Object.defineProperty(t, '__esModule', { value: !0 }), (t.hello = n), (t.default = void 0);
+    var u = n;
+    t.default = u;
+  },
+]);
