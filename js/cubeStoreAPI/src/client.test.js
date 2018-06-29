11ad35965f5ded6b3907dfa447f00a4cc6309829
Jorge <jbernal0019@yahoo.es>
2018-06-29 16:30:51 -0400

    Add tests for getPlugin and getPlugins methods

--- js/cubeStoreAPI/src/client.test.js
+++ js/cubeStoreAPI/src/client.test.js
@@ -1,7 +1,5 @@
 import StoreClient from './client';
-//import Collection from './cjson';
 import { expect } from 'chai';
-//import Request from './request';
 
 // http://sinonjs.org/releases/v5.1.0/fake-xhr-and-server/
 
@@ -11,17 +9,64 @@ describe('StoreClient', () => {
   const store_url = 'http://localhost:8010/api/v1/';
   const client = new StoreClient(store_url, username, password);
 
-  it('can retrieve plugins given search params', done => {
+  const plugin_repr = {
+    name: 'simplefsapp',
+    creation_date: '2018-06-29T16:02:12.915896Z',
+    modification_date: '2018-06-29T16:02:12.915940Z',
+    dock_image: 'fnndsc/pl-simplefsapp',
+    public_repo: 'https://github.com/FNNDSC',
+    type: 'fs',
+    authors: 'FNNDSC (dev@babyMRI.org)',
+    title: 'Simple chris fs app',
+    category: '',
+    description: 'A simple chris fs app demo',
+    documentation: 'http://wiki',
+    license: 'Opensource (MIT)',
+    version: '0.1',
+    execshell: 'python3',
+    selfpath: '/usr/src/simplefsapp',
+    selfexec: 'simplefsapp.py',
+    min_number_of_workers: 1,
+    max_number_of_workers: 1,
+    min_cpu_limit: 1000,
+    max_cpu_limit: 2147483647,
+    min_memory_limit: 200,
+    max_memory_limit: 2147483647,
+    min_gpu_limit: 0,
+    max_gpu_limit: 0,
+    parameters: [
+      {
+        name: 'dir',
+        type: 'path',
+        optional: true,
+        default: './',
+        flag: '--dir',
+        action: 'store',
+        help: 'look up directory',
+      },
+    ],
+  };
+
+  it('can retrieve a plugin given its name', function(done) {
+    const resp = client.getPlugin('simplefsapp');
+
+    resp
+      .then(plugin => {
+        //window.console.log('plugin: ', plugin);
+        expect(plugin).to.deep.equal(plugin_repr);
+      })
+      .then(done, done);
+  });
+
+  it('can retrieve plugins given search params', function(done) {
     const searchParams = { type: 'fs' };
     const resp = client.getPlugins(searchParams);
 
+    this.timeout(10000); // mocha test timeout, don't work with arrow functions
+
     resp
       .then(plugins => {
-        window.console.log('plugins: ', plugins);
-        window.console.log('plugins length: ', plugins.length);
-      })
-      .catch(error => {
-        window.console.log('Store error: ', error);
+        expect(plugins.length).to.equal(3);
       })
       .then(done, done);
   });
