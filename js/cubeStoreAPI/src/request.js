bf709b2360a8b8af125286bbad57c44c8552938c
Jorge <jbernal0019@yahoo.es>
2018-07-05 15:42:36 -0400

    Add _getConfig method and handle requests based on an auth token

--- js/cubeStoreAPI/src/request.js
+++ js/cubeStoreAPI/src/request.js
@@ -1,7 +1,7 @@
 /** * Imports ***/
 import axios from 'axios';
-import StoreRequestException from './exception';
 import Collection from './cj';
+import StoreRequestException from './exception';
 
 /**
  * Http request object.
@@ -26,24 +26,12 @@ export default class Request {
    * @return {*}
    */
   get(url, params) {
-    const config = {
-      method: 'get',
-      headers: {
-        Accept: this.contentType,
-        'content-type': this.contentType,
-      },
-      url: url,
-      params: params,
-      timeout: this.timeout,
-    };
-
-    if (this.auth) {
-      config.auth = this.auth;
-    }
+    const config = this._getConfig(url, 'get');
+    config.params = params;
 
     return axios(config)
       .then(response => {
-        return new Collection(response.data);
+        return response.data;
       })
       .catch(error => {
         Request._handleRequestError(error);
@@ -55,23 +43,12 @@ export default class Request {
    *
    * @param {*} url
    * @param {*} data
+   * @param {*} descriptorFile
    * @return {*}
    */
   post(url, data, descriptorFile) {
-    const config = {
-      method: 'post',
-      headers: {
-        Accept: this.contentType,
-        'content-type': this.contentType,
-      },
-      url: url,
-      data: data,
-      timeout: this.timeout,
-    };
-
-    if (this.auth) {
-      config.auth = this.auth;
-    }
+    const config = this._getConfig(url, 'post');
+    config.data = data;
 
     if (descriptorFile) {
       config['content-type'] = 'application/x-www-form-urlencoded';
@@ -79,11 +56,38 @@ export default class Request {
 
     return axios(config)
       .then(response => {
-        return new Collection(response.data);
+        return response.data;
       })
       .catch(error => {
         Request._handleRequestError(error);
       });
+  }
+
+  /**
+   * Internal method to create a config file for axios.
+   *
+   * @param {*} url
+   * @param {*} method
+   * @return {*}
+   */
+  _getConfig(url, method) {
+    const config = {
+      url: url,
+      method: method,
+      headers: {
+        Accept: this.contentType,
+        'content-type': this.contentType,
+      },
+      timeout: this.timeout,
+    };
+
+    if (this.auth && this.auth.username && this.auth.password) {
+      config.auth = this.auth;
+    } else if (this.auth && this.auth.token) {
+      config.headers.Authorization = 'Token ' + this.auth.token;
+    }
+
+    return config;
   }
 
   /**
@@ -92,25 +96,31 @@ export default class Request {
    * @param {*} error
    */
   static _handleRequestError(error) {
+    let errMsg;
+
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
       //console.log(error.response.data);
       //console.log(error.response.status);
       //console.log(error.response.headers);
-      const coll = new Collection(error.response.data);
-      throw new StoreRequestException(coll.getErrorMessage());
+      errMsg = error.response;
+      if (error.response.data.collection) {
+        errMsg = Collection.getErrorMessage(error.response.data.collection);
+      }
     } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
       //console.log(error.request);
-      throw new StoreRequestException(error.request);
+      errMsg = error.request;
     } else {
       // Something happened in setting up the request that triggered an Error
       //console.log('Error', error.message);
-      throw new StoreRequestException(error.message);
+      errMsg = error.message;
     }
+
+    throw new StoreRequestException(errMsg);
     //console.log(error.config);
   }
 }
