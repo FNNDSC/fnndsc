152af7fb91f4e77cc4941196603832ae2d23a85a
Jorge <jbernal0019@yahoo.es>
2018-07-06 16:28:32 -0400

    Change exports

--- js/chrisStoreAPI/src/index.js
+++ js/chrisStoreAPI/src/index.js
@@ -1,1 +1,7 @@
-export * from './client';
+import StoreClient from './client';
+import Request from './request';
+import Collection from './cj';
+
+export { StoreClient };
+export { Request };
+export { Collection };
