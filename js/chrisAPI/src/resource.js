0f317fa1ceec05b0902450bf17649d156ca9570a
Jorge <jbernal0019@yahoo.es>
2018-10-03 16:24:33 -0400

    Minimize number of neccessary API requests by making the data associated to collection items immediately available to item resource objects created from a list resource object

--- js/chrisAPI/src/resource.js
+++ js/chrisAPI/src/resource.js
@@ -17,15 +17,125 @@ class Resource {
    */
   constructor(resourceUrl, auth) {
     this.url = resourceUrl;
-    this.queryUrl = '';
     this.auth = auth;
     this.contentType = 'application/vnd.collection+json';
     this.collection = null;
+  }
+}
+
+export class ItemResource extends Resource {
+  /**
+   * Constructor
+   *
+   * @param {*} itemUrl
+   * @param {*} auth
+   */
+  constructor(itemUrl, auth) {
+    super(itemUrl, auth);
+
+    this.item = null;
+  }
+
+  /**
+   * Fetch this item resource from the REST API.
+   *
+   * @param {*} timeout
+   * @return {*}
+   */
+  get(timeout = 30000) {
+    const req = new Request(this.auth, this.contentType, timeout);
+    const self = this;
+    let url = this.url;
+
+    const result = req.get(url);
+
+    return new Promise((resolve, reject) => {
+      result
+        .then(response => {
+
+          // change the state of this object on successfull response
+          self.collection = null;
+          self.item = null;
+          if (response.data && response.data.collection) {
+            self.collection = response.data.collection;
+            self.item = self.collection.items[0];
+          }
+          resolve(self);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  /**
+   * Get the item's data descriptors
+   *
+   * @return {*}
+   */
+  get itemData() {
+    if (this.item) {
+      return Collection.getItemDescriptors(this.item);
+    }
+    return null;
+  }
+
+  /**
+   * Return true if the item resource object contains any item data.
+   *
+   * @return {*}
+   */
+  get isEmpty() {
+    if (this.item) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+  * Internal method to fetch a related resource from the REST API referenced by
+  * a link relation in the item object.
+   *
+   * @param {*} linkRelation
+   * @param {*} ResourceClass
+   * @param {*} params
+   * @param {*} timeout
+   * @return {*}
+   */
+  _getResource(linkRelation, ResourceClass, params = null, timeout = 30000) {
+    if (this.item) {
+      const urls = Collection.getLinkRelationUrls(this.item, linkRelation);
+
+      if (urls.length) {
+        const resourceUrl = urls[0];
+        const resourceObj = new ResourceClass(resourceUrl, this.auth);
+
+        return resourceObj.get(params, timeout);
+      } else {
+        const errMsg = 'Missing "' + linkRelation + '" link relation!';
+        throw new RequestException(errMsg);
+      }
+    }
+    return Promise.reject("Item object has not been set!");
+  }
+}
+
+export class ListResource extends Resource {
+  /**
+   * Constructor
+   *
+   * @param {*} listUrl
+   * @param {*} auth
+   */
+  constructor(listUrl, auth) {
+    super(listUrl, auth);
+
+    this.queryUrl = '';
     this.searchParams = null;
   }
 
   /**
-   * Fetch this resource from the REST API.
+   * Fetch this list resource from the REST API.
    *
    * @param {*} params
    * @param {*} timeout
@@ -71,34 +181,7 @@ class Resource {
   }
 
   /**
-   * Internal method to fetch a related resource referenced by a link relation from the
-   * REST API.
-   *
-   * @param {*} linkRelation
-   * @param {*} ResourceClass
-   * @param {*} params
-   * @param {*} timeout
-   * @return {*}
-   */
-  _getResource(linkRelation, ResourceClass, params = null, timeout = 30000) {
-    if (this.collection) {
-      const urls = Collection.getLinkRelationUrls(this.collection, linkRelation);
-
-      if (urls.length) {
-        const resourceUrl = urls[0];
-        const resourceObj = new ResourceClass(resourceUrl, this.auth);
-
-        return resourceObj.get(params, timeout);
-      } else {
-        const errMsg = 'Missing "' + linkRelation + '" link relation!';
-        throw new RequestException(errMsg);
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Return true if the resource object contains any item data.
+   * Return true if the list resource object contains any item data.
    *
    * @return {*}
    */
@@ -107,71 +190,6 @@ class Resource {
       return false;
     }
     return true;
-  }
-}
-
-export class ItemResource extends Resource {
-  /**
-   * Constructor
-   *
-   * @param {*} itemUrl
-   * @param {*} auth
-   */
-  constructor(itemUrl, auth) {
-    super(itemUrl, auth);
-  }
-
-  /**
-   * Get the item's data descriptors
-   *
-   * @return {*}
-   */
-  get itemData() {
-    if (this.collection && this.collection.items.length) {
-      return Collection.getItemDescriptors(this.collection.items[0]);
-    }
-    return null;
-  }
-}
-
-export class ListResource extends Resource {
-  /**
-   * Constructor
-   *
-   * @param {*} listUrl
-   * @param {*} auth
-   */
-  constructor(listUrl, auth) {
-    super(listUrl, auth);
-  }
-
-  /**
-   * Internal method to get the list of item objects.
-   *
-   * @param {*} ItemClass
-   * @return {*}
-   */
-  _getItems(ItemClass) {
-    if (this.collection) {
-      const items = this.collection.items;
-
-      return items.map(item => new ItemClass(item.href, this.auth));
-    }
-    return null;
-  }
-
-  /**
-   * Get the list of items' data descriptors.
-   *
-   * @return {*}
-   */
-  get itemsData() {
-    if (this.collection) {
-      const items = this.collection.items;
-
-      return items.map(item => Collection.getItemDescriptors(item));
-    }
-    return null;
   }
 
   /**
@@ -224,5 +242,52 @@ export class ListResource extends Resource {
     this.collection = null;
     this.searchParams = null;
     return Promise.resolve(this);
+  }
+
+  /**
+   * Internal method to get the list of item objects.
+   *
+   * @param {*} ItemClass
+   * @return {*}
+   */
+  _getItems(ItemClass) {
+    if (this.collection) {
+      const items = this.collection.items;
+
+      return items.map(item => {
+        const itemResource = new ItemClass(item.href, this.auth);
+
+        itemResource.collection = this.collection;
+        itemResource.item = item;
+      });
+    }
+    return null;
+  }
+
+  /**
+   * Internal method to fetch a related resource from the REST API referenced by
+   * a link relation in the collection object.
+   *
+   * @param {*} linkRelation
+   * @param {*} ResourceClass
+   * @param {*} params
+   * @param {*} timeout
+   * @return {*}
+   */
+  _getResource(linkRelation, ResourceClass, params = null, timeout = 30000) {
+    if (this.collection) {
+      const urls = Collection.getLinkRelationUrls(this.collection, linkRelation);
+
+      if (urls.length) {
+        const resourceUrl = urls[0];
+        const resourceObj = new ResourceClass(resourceUrl, this.auth);
+
+        return resourceObj.get(params, timeout);
+      } else {
+        const errMsg = 'Missing "' + linkRelation + '" link relation!';
+        throw new RequestException(errMsg);
+      }
+    }
+    return Promise.reject("Collection object has not been set!");
   }
 }
