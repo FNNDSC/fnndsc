41a36d3d0a78a981e92c2f65a81d07b0fbfcb18b
Jorge <jbernal0019@yahoo.es>
2018-11-29 12:39:19 -0500

    Update cj module code to the same as the cj module in chrisAPI

--- js/chrisStoreAPI/src/cj.js
+++ js/chrisStoreAPI/src/cj.js
@@ -1,13 +1,12 @@
 /**
- * Collection+Json object.
- *
- * @module cj
+ * Collection+Json utility object.
  */
 export default class Collection {
   /**
    * Get the error message from the collection object.
    *
-   * @return {*}
+   * @param {Object} collection - Collection+Json collection object
+   * @return {string} - error message
    */
   static getErrorMessage(collection) {
     if (collection.error) {
@@ -17,11 +16,11 @@ export default class Collection {
   }
 
   /**
-   * Get the list of urls for a link relation in a collection or item object.
+   * Get the list of urls for a link relation from a collection or item object.
    *
-   * @param {*} obj
-   * @param {*} relationName
-   * @return {*}
+   * @param {Object} obj - Collection+Json collection or item object
+   * @param {string} relationName - name of the link relation
+   * @return {string[]} - list of urls
    */
   static getLinkRelationUrls(obj, relationName) {
     const links = obj.links.filter(link => {
@@ -31,10 +30,10 @@ export default class Collection {
   }
 
   /**
-   * Get an item's data (descriptors) in an object.
+   * Get an item's data (descriptors).
    *
-   * @param {*} item
-   * @return {*}
+   * @param {Object} item - Collection+Json item object
+   * @return {Object} - object whose properties and values are the item's descriptor names and values respectively
    */
   static getItemDescriptors(item) {
     const itemObj = {};
@@ -44,5 +43,55 @@ export default class Collection {
       itemObj[descriptor.name] = descriptor.value;
     }
     return itemObj;
+  }
+
+  /**
+   * Get the url of the representation given by a collection obj.
+   *
+   * @param {Object} collection - Collection+Json collection object
+   * @return {string} - url of the resource representation
+   */
+  static getUrl(collection) {
+    return collection.href;
+  }
+
+  /**
+   * Get the list of descriptor names within a collection's template object.
+   *
+   * @param {Object} template - Collection+Json template object
+   * @return {string[]} - list of descriptor names
+   */
+  static getTemplateDescriptorNames(template) {
+    return template.data.map(descriptor => descriptor.name);
+  }
+
+  /**
+   * Get the list of descriptor names within a Collection+Json query array.
+   *
+   * @param {Object[]} queryArr - Collection+Json query array
+   * @return {string[]} - list of query parameter names
+   */
+  static getQueryParameters(queryArr) {
+    return queryArr[0].data.map(descriptor => descriptor.name);
+  }
+
+  /**
+   * Make a Collection+Json template object from a regular object whose properties are
+   * the item descriptors.
+   *
+   * @param {Object} descriptorsObj - item descriptors object
+   * @return {Object} - template object
+   */
+  static makeTemplate(descriptorsObj) {
+    const template = { data: [] };
+
+    let i = 0;
+    for (let property in descriptorsObj) {
+      if (descriptorsObj.hasOwnProperty(property)) {
+        template.data[i] = { name: property, value: descriptorsObj[property] };
+      }
+      i++;
+    }
+    return template;
   }
 }
