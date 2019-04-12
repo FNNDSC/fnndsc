e435a16a6e628a8947cd2bc0869a50f8d40bcdf5
Jorge <jbernal0019@yahoo.es>
2019-04-12 11:43:00 -0400

    ESDoc anotations to public properties.

--- js/chrisStoreAPI/src/exception.js
+++ js/chrisStoreAPI/src/exception.js
@@ -6,8 +6,14 @@
 export default class RequestException extends Error {
   constructor(...args) {
     super(...args);
+
+    /** @type {string} */
     this.name = this.constructor.name;
+
+    /** @type {Object} */
     this.request = null;
+
+    /** @type {Object} */
     this.response = null;
   }
 }
