46d468b4c34e17d71efd051252abda57405522ea
Alisamar Husain <zrthxn@gmail.com>
2021-07-29 19:52:48 +0530

    Single file for types

--- js/chrisAPI/dist/types/chrisinstance.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * ChRIS instance item resource object uniquely representing a ChRIS instance.
- */
-export default class ChrisInstance extends ItemResource {
-}
-import { ItemResource } from "./resource";
