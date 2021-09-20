e4a22a445a6cae0a0ab18d836a470416db77a15d
Jorge <jbernal0019@yahoo.es>
2021-09-20 19:43:45 -0400

    Move curl command to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+# create a test feed
+- curl -u cube:cube1234 -XPOST -H 'Content-Type: application/vnd.collection+json' -d '{"template":{"data":[{"name":"dir","value":"cube/"}]}}' 'http://localhost:8000/api/v1/plugins/1/instances/'
+- sleep 10
+
 cd $TRAVIS_BUILD_DIR/js/chrisStoreAPI
 yarn install
 yarn test
+
 cd $TRAVIS_BUILD_DIR/js/chrisAPI
 yarn install
 yarn test
