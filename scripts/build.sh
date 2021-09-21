10c26fde5c7654c7cb3ea43813b654b279dc3feb
Jorge <jbernal0019@yahoo.es>
2021-09-20 20:16:44 -0400

    Update TravisCI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # create a test feed
-- curl -u cube:cube1234 -XPOST -H 'Content-Type: application/vnd.collection+json' -d '{"template":{"data":[{"name":"dir","value":"cube/"}]}}' 'http://localhost:8000/api/v1/plugins/1/instances/'
-- sleep 100
+curl -u cube:cube1234 -XPOST -H 'Content-Type: application/vnd.collection+json' -d '{"template":{"data":[{"name":"dir","value":"cube/"}]}}' 'http://localhost:8000/api/v1/plugins/1/instances/'
+sleep 30
 
 cd $TRAVIS_BUILD_DIR/js/chrisStoreAPI
 yarn install
