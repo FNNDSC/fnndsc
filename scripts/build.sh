0d632656bd0679c3b3b385a5a94a087bb9ec8805
Jorge <jbernal0019@users.noreply.github.com>
2021-09-20 21:25:42 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # create a test feed
-curl -u cube:cube1234 -XPOST -H 'Content-Type: application/vnd.collection+json' -d '{"template":{"data":[{"name":"dir","value":"cube/"}]}}' 'http://localhost:8000/api/v1/plugins/1/instances/'
-sleep 30
+sleep 60
+curl -u cube:cube1234 -XPOST -H 'Content-Type: application/vnd.collection+json' -d '{"template":{"data":[{"name":"dir","value":"cube/"}]}}' http://localhost:8000/api/v1/plugins/1/instances/
+sleep 120
 
 cd $TRAVIS_BUILD_DIR/js/chrisStoreAPI
 yarn install
