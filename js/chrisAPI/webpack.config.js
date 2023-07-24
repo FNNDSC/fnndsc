50d56e5e9bfbe2006a48fcc1d87c9f8efb920a41
PintoGideon <gideonpinto123@gmail.com>
2023-07-24 14:05:43 -0400

    Trying webpack

--- js/chrisAPI/webpack.config.js
+++ js/chrisAPI/webpack.config.js
@@ -3,20 +3,16 @@ const merge = require('webpack-merge');
 
 let baseConfig = {
   // Change to your "entry-point".
-  entry: './src/index.js',
+  entry: './src/index',
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
   resolve: {
     extensions: ['.js', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        // Include ts, tsx, and js files.
-        test: /\.(tsx?)|(js)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
   },
 };
 
@@ -24,10 +20,7 @@ let targets = ['web', 'node'].map((target) => {
   let base = merge(baseConfig, {
     target: target,
     output: {
-      library: 'ChRISAPI',
-      libraryTarget: 'umd',
-      filename: target === 'node' ? 'chrisapi-server.js' : 'chrisapi-client.js',
-      path: path.resolve(__dirname, './dist'),
+      path: path.resolve(__dirname, './dist/' + target),
     },
   });
   return base;
